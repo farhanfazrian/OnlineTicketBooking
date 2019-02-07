78a5c40b12bae032196f2b9ad27b81e3949a5345
Farhan Fariz Fazrian <farhanfazrian@gmail.com>
2019-02-07 23:17:44 +0700

    Add files via upload

---
+++ TiketBioskop/QueryInsert.sql
@@ -0,0 +1,2 @@
+INSERT INTO `tiket_bioskop`.`customer` (`id_customer`, `name`, `phone`, `email`, `password`) VALUES ('3', 'User3', '08921023125', 'user3@gmail.com', '$2a$10$EblZqNptyYvcLm/VwDCVAuBjzZOI7khzdyGPBr08PpIi0na624b8.');
+INSERT INTO `tiket_bioskop`.`ticket` (`id_ticket`, `film`, `date`, `start_time`, `end_time`) VALUES ('5', 'Spiderman', '2019-02-10', '19:00:00', '21:00:00');
